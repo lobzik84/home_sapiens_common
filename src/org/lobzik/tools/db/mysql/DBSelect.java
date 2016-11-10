ab044cd776e4492b01e141c29f89fa02bbd57cfe
lobzik <lobzik@lobzik-SATELLITE-R830>
2016-11-10 21:23:56 +0300

    Merge origin/master
    Conflicts:
    	sql/dump.sql
    	src/java/org/lobzik/home_sapiens/pi/modules/BehaviorModule.java

--- src/org/lobzik/tools/db/mysql/DBSelect.java
+++ src/org/lobzik/tools/db/mysql/DBSelect.java
@@ -133,7 +133,6 @@ public class DBSelect
           case Types.NUMERIC:
           case Types.SMALLINT:
           case Types.DECIMAL:
-          case Types.DOUBLE:
             int scale = rsmd.getScale(i);
             if (scale > 0)
             {
